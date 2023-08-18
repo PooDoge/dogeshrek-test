93c056ceb4b305c25f2a5fc65f0cf4b9f2188b80
PooDoge <112787253+PooDoge@users.noreply.github.com>
2023-08-18 19:31:21 +0000

    fix: Small changes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -26,7 +26,7 @@ const WETH_ONLY: ChainTokenList = {
 // used to construct intermediary pairs for trading
 export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DOGECORN, DOGESHREK, DC, KIBBY, DTOOLS, RAINGOD]
+  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DOGECORN, DOGESHREK, DC, KIBBY]
 }
 
 /**
